f136210d85bf323dde8ace4fdf4efc86b31c7ae1
Bryan Clark <me@moenzuel.com>
2022-11-08 20:26:04 -0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e # -e: exit on error
+
+# ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+cat <<EOF >> ~/.zshrc
+# ASDF
+. $HOME/.asdf/asdf.sh
+# append completions to fpath
+fpath=(${ASDF_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit
+EOF
+
+# Chezmoi
+asdf plugin add chezmoi
+asdf install chezmoi latest
+asdf global chezmoi latest
+
+# Starship
+asdf plugin add starship
+asdf install starship latest
+asdf global starship latest
+cat <<EOF >> ~/.zshrc
+# Starship
+eval "$(asdf exec starship init zsh)" 
+EOF
+
+# direnv
+asdf plugin-add direnv
+asdf direnv setup --shell bash --version latest
+
