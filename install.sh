18c62b18f308d7897d9c9341ec42b97543d92481
Bryan Clark <bryan@funraise.io>
2022-11-16 14:53:11 -0800

    wip

--- install.sh
+++ install.sh
@@ -2,37 +2,29 @@
 
 set -e # -e: exit on error
 
+# Chezmoi
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+fi
+
+chezmoi init --apply moenzuel
+
 # ASDF
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-cat <<EOF >> ~/.zshrc
-# ASDF
-. $HOME/.asdf/asdf.sh
-# append completions to fpath
-fpath=(${ASDF_DIR}/completions $fpath)
-# initialise completions with ZSH's compinit
-autoload -Uz compinit && compinit
-EOF
-
-# Chezmoi
-asdf plugin add chezmoi
-asdf install chezmoi latest
-asdf global chezmoi latest
-
-# Starship
-asdf plugin add starship
-asdf install starship latest
-asdf global starship latest
-cat <<EOF >> ~/.zshrc
-# Starship
-eval "$(asdf exec starship init zsh)" 
-EOF
+# cut -d\ -f1 .tool-versions|grep -E '^[^#]'|xargs -L1 asdf plugin add
+asdf install
 
 # direnv
-asdf plugin-add direnv
 asdf direnv setup --shell bash --version latest
 
 # 1Password-CLI
-asdf plugin add 1password-cli
-asdf install 1password-cli latest
-asdf global 1password-cli latest
+
 
