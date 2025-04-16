42036200a5e206f9488629cee2168279af4571da
Bryan Clark <me@moenzuel.com>
2025-04-15 22:35:28 -0700

    Add source skip for mise completion source in souce compltions hook

--- home/dot_config/fish/conf.d/source-completions.fish
+++ home/dot_config/fish/conf.d/source-completions.fish
@@ -1,5 +1,8 @@
 function source_completions --on-variable LOCAL_FISH_COMPLETION_DIR
     for cf in $LOCAL_FISH_COMPLETION_DIR/*.fish
+        if test (basename $cf) = "mise.fish"
+            continue
+        end
         source < $cf
     end
 end
